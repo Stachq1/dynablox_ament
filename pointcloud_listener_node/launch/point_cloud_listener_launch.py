a8167071b48cbce51ade185414bcbc0e5d20314d
Stachq1 <stanislawpiasecki@protonmail.ch>
2024-09-25 15:58:35 +0200

    Fix typos related to config file

--- pointcloud_listener_node/launch/point_cloud_listener_launch.py
+++ pointcloud_listener_node/launch/point_cloud_listener_launch.py
@@ -5,12 +5,12 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    package = get_package_share_directory("pointcloud_listener")
+    package = get_package_share_directory("pointcloud_listener_node")
     config_path = os.path.join(package, "config", "config.yaml")
 
     return LaunchDescription([
         Node(
-            package='pointcloud_listener',
+            package='pointcloud_listener_node',
             executable='pointcloud_listener_node',
             output='screen',
             parameters=[config_path]
